6ce9577742c793d6eadb34224aaf423893f63880
alabbar <abdullahalabbar920@gmail.com>
2025-05-03 15:35:39 +0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
-pub mod error;
 pub mod lexer;
 pub mod parser;
 pub mod vm;
 
-pub use error::{Error, Result};
 pub use lexer::{Lexer, Token};
-pub use parser::{ASTNode, Parser};
-pub use vm::{VM, Opcode};
+pub use parser::{Parser, ParseError, ASTNode};
