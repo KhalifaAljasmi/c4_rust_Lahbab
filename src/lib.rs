1ad8c93cccd06bc3f0e3fd3b947595603cc30ae5
alabbar <abdullahalabbar920@gmail.com>
2025-05-04 16:19:01 +0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+// src/lib.rs
+
+pub mod error;
 pub mod lexer;
 pub mod parser;
+pub mod codegen;
 pub mod vm;
 
+pub use error::{Error, Result};
 pub use lexer::{Lexer, Token};
-pub use parser::{Parser, ParseError, ASTNode};
-pub use vm::{VM, Value, RuntimeError};
+pub use parser::{Parser, Decl, Type, Stmt, Expr, UnaryOp, BinaryOp, EnumVariant};
+pub use codegen::CodeGen;
+pub use vm::{VM, Opcode};
